8b2c452fb34c091bca417900500610bdab1c705b
shivam2823 <snaik@canqualify.com>
2025-06-26 13:39:35 +0530

    add the change

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/resume")
+def resume():
+    return send_file("resume.pdf", as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
